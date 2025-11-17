f12792b91062a9c2f7eab479a6367d523acbe211
Jeffrey S Whitaker <jwhitake@hercules-login-2.hpc.msstate.edu>
2025-11-17 12:30:21 -0600

    update

--- setup_expt.sh
+++ setup_expt.sh
@@ -4,4 +4,13 @@ mkdir FV3ATM_OUTPUT
 /bin/cp -f FV3ATM_OUTPUT_control/*F06* FV3ATM_OUTPUT
 /bin/rm -rf FV3_RESTART
 mkdir FV3_RESTART
-/bin/cp -f FV3_RESTART_control/${datestring}*nc FV3_RESTART
+#/bin/cp -f FV3_RESTART_control/${datestring}*nc FV3_RESTART
+cd FV3_RESTART_control
+for file in ${datestring}*nc; do
+   file2=`echo $file | cut -f3-10 -d"."`
+   /bin/ln -fs $PWD/$file ../FV3_RESTART/$file
+   if [ $? -ne 0 ]; then
+     echo "restart file missing..."
+     exit 1
+   fi
+done
