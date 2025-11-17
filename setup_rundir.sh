fd45a8ff117a3d0458621b2562c99a0ae6568a02
Jeffrey S Whitaker <jwhitake@hercules-login-2.hpc.msstate.edu>
2025-11-17 12:28:03 -0600

    update

--- setup_rundir.sh
+++ setup_rundir.sh
@@ -3,9 +3,19 @@ export USHgfs=$HOMEgfs/ush
 
 source $HOMEgfs/dev/ush/gw_setup.sh
 
-PSLOT="C48_ATM"
-export current_cycle=2021032312 #C48, C96
-export previous_cycle=`incdate $current_cycle -6`
+cuberes=1152
+PSLOT="C${cuberes}_ATM"
+do_iau=${do_iau:-"NO"}
+if [ $do_iau == "YES" ]; then
+   export current_cycle=2025091503 #C48, C96
+   export previous_cycle=`incdate $current_cycle -3`
+   #ICSDIR="/work/noaa/gsienkf/whitaker/ICSDIR"
+   ICSDIR="$PWD/RESTART"
+else
+   export current_cycle=2025091500 #C48, C96
+   export previous_cycle=`incdate $current_cycle -6`
+   ICSDIR="/work/noaa/gsienkf/whitaker/ICSDIR"
+fi
 YYYYMMDD=`echo $current_cycle | cut -c1-8`
 HH=`echo $current_cycle | cut -c9-10`
 export PDY=$YYYYMMDD
@@ -14,10 +24,10 @@ TESTDIR="/work/noaa/gsienkf/whitaker/GWTESTS"
 /bin/rm -rf $TESTDIR/EXPDIR/$PSLOT
 /bin/rm -rf $TESTDIR/COMROOT/$PSLOT
 CONFIGDIR=$TESTDIR/EXPDIR/$PSLOT
-ICSDIR="/work2/noaa/global/role-global/data/ICSDIR"
-EXPTYAML=$HOMEgfs/dev/ci/cases/pr/C48_ATM.yaml
+EXPTYAML="$HOMEgfs/dev/ci/cases/pr/C${cuberes}_ATM.yaml"
 
-pslot=$PSLOT HPC_ACCOUNT="gsienkf" RUNTESTS=$TESTDIR ICSDIR_ROOT=$ICSDIR $HOMEgfs/dev/workflow/create_experiment.py --yaml $EXPTYAML
+pslot=$PSLOT HPC_ACCOUNT="gsienkf" RUNTESTS=$TESTDIR ICSDIR_ROOT=$ICSDIR $HOMEgfs/dev/workflow/create_experiment.py --yaml $EXPTYAML 
+
 
 source $CONFIGDIR/config.base
 source $CONFIGDIR/config.fcst
@@ -35,20 +45,49 @@ source "${USHgfs}/parsing_ufs_configure.sh"      # include functions for ufs_con
 source "${USHgfs}/parsing_model_configure_FV3.sh"
 source "${USHgfs}/atparse.bash"
 
-export DOIAU="NO"
-export FHMIN=0
-export FHMAX=9
-export FHOUT=3
-export FHZERO=6
-export FV3_RESTART_FH="3 -1"
+do_iau=${do_iau:-"NO"}
+export DOIAU=$do_iau
+
+if [ $DOIAU == "YES" ]; then
+   export FHMIN=3
+   export FHMAX=9 
+   export FHOUT=3
+   export IAU_INC_FILES="'fv3_increment.nc'"
+   export IAU_FILTER_INCREMENTS=.false.
+   export IAUFHRS="6"
+   export IAU_OFFSET=0
+   export IAU_FHROT=3
+   export FV3_RESTART_FH="6"
+else
+   export FHMIN=0
+   export FHMAX=9
+   export FHOUT=3
+   export IAU_INC_FILES="''"
+   export FV3_RESTART_FH="3"
+fi
+
+export FHZERO=3
 export FHOUT_HF=0
 export FHMAX_HF=0
-export layout_x=2
-export layout_y=2
+
+# C384
+if [ $cuberes -eq 384 ]; then
+export layout_x=8
+export layout_y=8
 export WRITE_GROUP=1
-export WRTTASK_PER_GROUP=24
-export ATMPETS=48
+export WRTTASK_PER_GROUP=96
+export ATMPETS=480    
 export ATMTHREADS=1
+elif [ $cuberes -eq 1152 ]; then
+# C1152
+export layout_x=24
+export layout_y=24
+export WRITE_GROUP=4
+export WRTTASK_PER_GROUP=96
+export ATMPETS=3840   
+export ATMTHREADS=2
+echo "C1152"
+fi
 
 export DATA=$PWD
 export DATAoutput=$DATA
@@ -58,6 +97,14 @@ export DATArestart=$DATA
 /bin/rm -rf INPUT
 mkdir INPUT
 FV3_predet
+if [ $DOIAU == "YES" ]; then
+   echo "warm start with IAU"	 
+   export warm_start=".true."
+   export nggps_ic=".false."
+   export ncep_ic=".false."
+   export external_ic=".false."
+   export mountain=".true."
+fi
 /bin/rm -rf model_configure
 FV3_model_configure
 /bin/rm -rf input.nml
@@ -70,5 +117,21 @@ UFS_configure
 ln -fs $HOMEgfs/exec/gfs_model.x .
 
 cd INPUT
-ln -fs $ICSDIR/$CASE/20250808/gfs.$YYYYMMDD/$HH/model/atmos/input/gfs*nc .
-ln -fs $ICSDIR/$CASE/20250808/gfs.$YYYYMMDD/$HH/model/atmos/input/sfc*nc .
+if [ $DOIAU == "YES" ]; then
+   #ln -fs $ICSDIR/C$cuberes/gfs.$YYYYMMDD/$HH/model/atmos/restart/*nc .
+   inputdir=$PWD
+   pushd ../FV3_RESTART_control
+   datestring="${YYYYMMDD}.${HH}"
+   for file in ${ICSDIR}/${datestring}*nc; do
+      file2=`echo $file | cut -f3-10 -d"."`
+      /bin/ln -fs $file $inputdir/$file2
+      if [ $? -ne 0 ]; then
+         echo "restart file missing..."
+         exit 1
+      fi
+   done
+   popd
+else
+   ln -fs $ICSDIR/C$cuberes/gdas.$YYYYMMDD/$HH/model/atmos/input/gfs*nc .
+   ln -fs $ICSDIR/C$cuberes/gdas.$YYYYMMDD/$HH/model/atmos/input/sfc*nc .
+fi
